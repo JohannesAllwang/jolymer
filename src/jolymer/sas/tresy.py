fc853cd540fd2b0dea3f0ed9ae3516cea4762c34
Johannes Allwang <johannes.allwang@ph.tum.de>
2021-08-24 16:44:24 +0200

    for aris poster

--- src/jolymer/sas/tresy.py
+++ src/jolymer/sas/tresy.py
@@ -43,7 +43,7 @@ def plot_tresyfits(tresy_numbers, model, p0, bounds, fixed_pars={}):
                             p0=p0, iqmin=iqmin, fixed_parameters=fixed_pars)
         # label = f'{m.sample.get_NPname()} pH {m.sample.buffer.pH} {m.sample.buffer.salt_concentration} salt'
         label = f'{m.sample.get_NPname()} pH {m.sample.buffer.pH} c({ m.sample.PS.short_name })= {m.sample.PS_gpl}'
-        label = f'pH {m.sample.buffer.pH} c({ m.sample.PS.short_name })= {m.sample.PS_gpl}'
+        label = f'pH {m.sample.buffer.pH}; c({ m.sample.PS.short_name }) / c(TRY)= {m.sample.PS_gpl}'
         marker = '.'
         df = m.get_data(cout=False)[iqmin::]
         ax.errorbar(df.q, df.I, df.err_I, marker = marker, color=color,
@@ -62,4 +62,40 @@ def plot_tresyfits(tresy_numbers, model, p0, bounds, fixed_pars={}):
     axes[1][0].set_xlabel('$q$ [1/nm]')
     axes[1][1].set_xlabel('$q$ [1/nm]')
     axes[1][0].set_ylabel('$I$ [1/cm]')
+    axes[0][0].set_ylabel('$I$ [1/cm]')
+
+
+def two_tresyfits(tresy_numbers, model, p0, bounds, fixed_pars={}):
+    meas_nums= tresy_numbers
+    fig, axes = plt.subplots(nrows=1, ncols=2, figsize = (12,5), 
+                             sharex=True, sharey=True, squeeze=False)
+    for i, ax in zip(tresy_numbers, axes.flatten()):
+        index = i-1
+        # iqmin=iqmins[i-1]
+        # color = cm[i-1]
+        m = get_m(i, 20)
+        color = 'blue'
+        iqmin=0
+        fit_dict, fit_df = model.fit(m, bounds=bounds, 
+                            p0=p0, iqmin=iqmin, fixed_parameters=fixed_pars)
+        # label = f'{m.sample.get_NPname()} pH {m.sample.buffer.pH} {m.sample.buffer.salt_concentration} salt'
+        label = f'{m.sample.get_NPname()} pH {m.sample.buffer.pH} c({ m.sample.PS.short_name })= {m.sample.PS_gpl}'
+        label = f'pH {m.sample.buffer.pH}; c({ m.sample.PS.short_name }) / c(TRY)= {m.sample.PS_gpl}'
+        marker = '.'
+        df = m.get_data(cout=False)[iqmin::]
+        ax.errorbar(df.q, df.I, df.err_I, marker = marker, color=color,
+                        linestyle='', label = label, elinewidth=0.2)
+        model.plot_fit(fit_df, (fig, ax), color='salmon')
+        ax.set_xscale('log')
+        ax.set_yscale('log')
+        ax.legend()
+        #$\\pm$ {3:.2f}
+        text=model.get_text(fit_dict)
+        ax.annotate(text, xy=(0.0, 0.0), 
+                    xycoords='axes fraction')
+        ax.set_ylim(1e-7, 1)
+        ax.grid()
+
+    axes[0][0].set_xlabel('$q$ [1/nm]')
+    axes[0][1].set_xlabel('$q$ [1/nm]')
     axes[0][0].set_ylabel('$I$ [1/cm]')
