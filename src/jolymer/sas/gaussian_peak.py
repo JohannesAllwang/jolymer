c7c57d52340a2cb346d5d99718837a582167ca90
Johannes Allwang <johannes.allwang@ph.tum.de>
2021-10-19 16:24:39 +0200

    more models for cs-try gel

--- src/jolymer/sas/gaussian_peak.py
+++ src/jolymer/sas/gaussian_peak.py
@@ -37,6 +37,7 @@ Authorship and Verification
 from . import SAXS_Model as sasmodel
 import numpy as np
 from scipy import special
+from .ornsteinZernike import oz
 
 name = "gaussian_peak"
 title = "Gaussian shaped peak"
@@ -52,8 +53,59 @@ parameters = [["peak_pos", "1/Ang", 0.05, [-np.inf, np.inf], "", "Peak position"
                "Peak width (standard deviation)"],
              ]
 
-def Iq(q, scale, peak_pos, sigma):
-    scaled_dq = (q - peak_pos)/sigma
+def Iq(q, peak_pos, sigma, scale):
+    scaled_dq = (q - peak_pos*np.ones(len(q)))/sigma
     return scale * np.exp(-0.5*scaled_dq*scaled_dq)
 
+class GaussianPeak(sasmodel.SAXS_Model):
+    
+    def __init__(self):
+        self.name = 'gaussian_peak'
+        self.longname = 'Gaussian Peak'
+        self.parameters = ['peak_pos', 'sigma', 'scale']
+        self.pdict = {'xi' : ['$\\xi$', 'nm']}
+        self.fitfunc = Iq
+
+    def get_text(self, fit_dict):
+        text = """
+        $R =$ {0:.4f} $\\pm$ {1:.2f} nm
+        $\\sigma =$ {2:.2f} $\\pm$ {3:.2f}
+        $A =$ {4:.2E}
+        $\\chi^2 = $ {6:.4}
+        """.format(fit_dict['peak_pos'], fit_dict['std_peak_pos'],
+                   fit_dict['sigma'], fit_dict['std_sigma'], 
+                   fit_dict['scale'], fit_dict['std_scale'],
+                  fit_dict['chi2'])
+        return text
+        
+
+
+background = sasmodel.Background()
+gaussian_peak = GaussianPeak()
+# gaussian_peak.get_text = get_text_be
+
+forward = sasmodel.Porod()
+forward.parameters = ['fw_scale', 'fw_exp']
+
+gaussian_peak_forward = gaussian_peak.plusmodel(forward, name='gaussian_peak_fw', longname = 'Beaucage')
+
+def get_text_gel(fit_dict):
+    text = """
+    $R =$ {0:.5f} $\\pm$ {1:.2f} nm
+    $\\sigma =$ {2:.2f} $\\pm$ {3:.2f}
+    $\\xi =$ {4:.2f} $\\pm$ {5:.2f}
+    $\\n_oz =$ {6:.2f} $\\pm$ {7:.2f}
+    $A =$ {8:.2E}
+    $A =$ {10:.2E}
+    $\\chi^2 = $ {12:.4}
+    """.format(fit_dict['peak_pos'], fit_dict['std_peak_pos'],
+               fit_dict['sigma'], fit_dict['std_sigma'], 
+               fit_dict['xi'], fit_dict['std_xi'], 
+               fit_dict['lorentz_exp'], fit_dict['std_lorentz_exp'], 
+               fit_dict['scale'], fit_dict['std_scale'],
+               fit_dict['lorentz_scale'], fit_dict['std_lorentz_scale'],
+              fit_dict['chi2'])
+    return text
+gel_model = gaussian_peak_forward.plusmodel(oz, name = 'get_model', longname = 'forward gaussian peak and oz')
+gel_model.get_text = get_text_gel
 
