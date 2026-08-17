aabbd5ba8de8b92c89e4536f3df7c96f16f22dee
Johannes Allwang <johannes@it-allwang.de>
2026-08-17 17:05:48 +0800

    correct path and include packate data

--- scripts/build_nuitka_onefile.ps1
+++ scripts/build_nuitka_onefile.ps1
@@ -2,7 +2,7 @@ python -m nuitka `
     --onefile `
     --enable-plugin=pyqt6 `
     --include-package=jolymer `
+    --include-package-data=jolymer `
     --output-dir=build `
     --output-filename=jolymer-secplot.exe `
-    src/jolymer/gui/app.py
-
+    src/jolymer/gui/sec_plot/app.py
