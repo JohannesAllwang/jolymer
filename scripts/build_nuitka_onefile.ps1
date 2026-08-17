1add583185f013030d4f56aa0f8f11ac2b6821e9
Johannes Allwang <johannes@it-allwang.de>
2026-08-17 18:12:35 +0800

    fixed data dependency

--- scripts/build_nuitka_onefile.ps1
+++ scripts/build_nuitka_onefile.ps1
@@ -3,6 +3,7 @@ python -m nuitka `
     --enable-plugin=pyqt6 `
     --include-package=jolymer `
     --include-package-data=jolymer `
+    --include-data-dir=src/jolymer/data=jolymer/data `
     --output-dir=build `
     --output-filename=jolymer-secplot.exe `
     src/jolymer/gui/sec_plot/app.py
