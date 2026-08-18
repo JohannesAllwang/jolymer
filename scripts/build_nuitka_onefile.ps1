3c5dc3d09c52835ccaa53d07c4eb9f5b3c1c3c47
Johannes Allwang <johannes@it-allwang.de>
2026-08-18 12:11:32 +0800

    update nuitka include package fabio

--- scripts/build_nuitka_onefile.ps1
+++ scripts/build_nuitka_onefile.ps1
@@ -2,6 +2,7 @@ python -m nuitka `
     --onefile `
     --enable-plugin=pyqt6 `
     --include-package=jolymer `
+    --include-package=fabio `
     --include-package-data=jolymer `
     --include-data-dir=src/jolymer/data=jolymer/data `
     --output-dir=build `
